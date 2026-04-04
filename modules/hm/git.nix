3654a1dd5b1a19db571e30f75d07fe0ed8b82118
eliot <eliotorderson@gmail.com>
2026-04-04 23:38:26 +0800

    Add git.nix global config

--- modules/hm/git.nix
+++ modules/hm/git.nix
@@ -1,19 +1,15 @@
 {
 
+  hydenix.hm.git.enable = false;
   programs.git = {
     enable = true;
     userName = "eliot";
     userEmail = "eliotorderson@gmail.com";
 
-    aliases = {
-      st = "status";
-      co = "checkout";
-      br = "branch";
-    };
-
     extraConfig = {
-      pull.rebase = false;
       init.defaultBranch = "main";
+      pull.rebase = false;
     };
   };
+
 }

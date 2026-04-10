499d0c986375100b711a025f71ae69ba2e678cf1
eliot <eliotorderson@gmail.com>
2026-04-10 20:07:10 +0800

    add zeditor config

--- modules/hm/path.nix
+++ modules/hm/path.nix
@@ -2,13 +2,14 @@
 
 let
 
-  _flakeConfigDir = "${config.home.homeDirectory}/hydenix/modules/hm/config";
   hyprland_shadres = "${config.home.homeDirectory}/hydenix/modules/hm/config/shaders/self_vibrance.frag";
+  zed_config = "${config.home.homeDirectory}/hydenix/modules/hm/config/zed";
+
 in
 {
-  # home.file.".claude/CLAUDE.md".source =
-  #   config.lib.file.mkOutOfStoreSymlink "${flakeConfigDir}/claude/CLAUDE.md";
   xdg.configFile."hypr/shaders/self_vibrance.frag".source = config.lib.file.mkOutOfStoreSymlink "${
     hyprland_shadres
   }";
+
+  xdg.configFile."zed".source = config.lib.file.mkOutOfStoreSymlink "${zed_config}";
 }
